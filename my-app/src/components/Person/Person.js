94aa68557f668a4d39c4bc81fe523523b0603565
József <pallagijoe@gmail.com>
2020-06-27 12:31:20 +0200

    fix update bug

--- my-app/src/components/Person/Person.js
+++ my-app/src/components/Person/Person.js
@@ -2,19 +2,20 @@ import React from 'react';
 import '../Person/Person.css';
 import styled from 'styled-components';
 
+const PersonDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+    
+    @media(min-width: 500px) {
+        width :450px;
+    }
+    `;
+
 const person = (props) => {
-    const PersonDiv = styled.div`
-        width: 60%;
-        margin: 16px auto;
-        border: 1px solid #eee;
-        box-shadow: 0 2px 3px #ccc;
-        padding: 16px;
-        text-align: center;
-        
-        @media(min-width: 500px) {
-            width :450px;
-        }
-        `;
     return (
         <PersonDiv>
             <p>I am a person</p>
